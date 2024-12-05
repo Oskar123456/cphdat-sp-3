37ff0996a36e0441f8fe28d8738afde0cb99013a
oskar123456 <cph-oh82@cphbusiness.dk>
2024-12-05 14:29:23 +0100

    uddate

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -32,7 +32,7 @@ function Trips({currentUser}) {
 
     useEffect(() => {
         console.log('Trips');
-        fetchWithJwt("http://localhost:9999/api/trips", initTrips);
+        fetchWithJwt("https://157.245.73.150:9999/api/trips", initTrips);
     }, [])
 
     return (

aba79bf495d794684a161aaa2e17104a4767d948
oskar123456 <cph-oh82@cphbusiness.dk>
2024-12-05 15:17:42 +0100

    uddate

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -9,7 +9,7 @@ function Trip({currentUser}) {
     let { "*": splat } = useParams();
 
     useEffect(() => {
-        fetchWithJwt("https://exam.obhnothing.dk/api/trips" + splat, setTrip, setError);
+        fetchWithJwt("https://exam.obhnothing.dk/api/trips/" + splat, setTrip, setError);
     }, [])
 
     return (

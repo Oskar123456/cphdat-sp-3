854dfaacc6e4eccf822397045cee5a31efe57a30
oskar123456 <cph-oh82@cphbusiness.dk>
2024-12-14 12:45:57 +0100

    oeu

--- src/js/Themes.js
+++ src/js/Themes.js
@@ -5,6 +5,12 @@ const Themes =
         white: "#FFFFFF",
         gray:  "#AAAAAA",
         red: "#EA4656"
+    },
+    {
+        black: "#000000",
+        white: "#FFFFFF",
+        gray:  "#AAAAAA",
+        red: "#EA4656"
     }
 ];
 

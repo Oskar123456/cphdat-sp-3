c582c1919526e1a4520acddcd2b9dd1befa815f6
oskar123456 <cph-oh82@cphbusiness.dk>
2024-12-17 13:02:17 +0100

    rm chartjs

--- src/components/PokemonDisplay.jsx
+++ src/components/PokemonDisplay.jsx
@@ -9,10 +9,6 @@ import { habitatImageLink } from "../js/PokeUtils.js";
 import PokemonImgDiv from "./PokemonImgDiv.jsx";
 import TypeList from './TypeList.jsx'
 import StatChart from './StatChart.jsx'
-//import Testchart from '../js/testchart.jsx'
-
-
-import { Chart } from 'chart.js'
 
 const StyledDiv = styled.div`
     font-family: "VT323", serif;
